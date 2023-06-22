a259423739728eaae7fdf102678c868a69d30698
Benson Lee <BensonL1334@gmail.com>
2023-06-22 01:22:38 -0400

    Edit scanner

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -10,8 +10,24 @@ import static com.craftinginterpreters.lox.TokenType.*;
 class Scanner {
   private final String source;
   private final List<Token> tokens = new ArrayList<>();
+  private int start = 0;
+  private int current = 0;
+  private int line = 1;
 
   Scanner(String source) {
     this.source = source;
+  }
+  List<Token> scanTokens() {
+    while(!isAtEnd()) {
+      start = current;
+      scanToken()
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+  }
+  
+  private boolean isAtEnd() {
+    return current >= source.length()
   }
 }
