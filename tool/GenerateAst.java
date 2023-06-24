6fa73b089cdce6dd32b8d720cf0b8c9f4cd4644a
Benson Lee <BensonL1334@gmail.com>
2023-06-23 23:33:49 -0400

    Add methods for defining classes

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -19,5 +19,24 @@ public class GenerateAst {
       "Unary    : Token operator, Expr right"
     ));
   }
+  private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException{
+    String path = outputDir + "/" + baseName + ".java";
+    PrintWriter writer = new PrintWriter(path, "UTF-8");
+
+    writer.println("package com.craftinginterpreters.lox");
+    writer.println();
+    writer.println("import java.util.List;");
+    writer.println();
+    writer.println("abstract class " + baseName + " {");
+
+    // The AST classes.
+    for (String type : types) {
+      String className = type.split(":")[0].trim();
+      String fields = type.split(":")[1].trim();
+      defineType(writer, baseName, className, fields);
+    }
+    writer.println("}");
+    writer.println();
+    }
 }
 
