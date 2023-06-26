b0de8c3b3800c83b0ed97f2365105f9d194392f3
Benson Lee <BensonL1334@gmail.com>
2023-06-26 14:46:06 -0400

    Add defineType for automate subclass

--- tool/GenerateAst.java
+++ tool/GenerateAst.java
@@ -38,5 +38,28 @@ public class GenerateAst {
     writer.println("}");
     writer.println();
     }
+  private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+    writer.println("  static class " + className + " extends " + baseName + " {");
+
+    // Constructuor
+    writer.println("    " + className + "(" + fieldList + ") {");
+
+    // Store parameters in fields.
+    String[] fields = fieldList.split(", ");
+    for (String field : fields) {
+      String name = field.split(" ")[1];
+      writer.println("    this." + name + "=" + name + ";");
+    }
+
+    writer.println("    }");
+
+    //Fields
+    writer.println();
+    for (String field : fields) {
+      writer.println("    final " + field + ";");
+    }
+
+    writer.println("  }");
+  }
 }
 
