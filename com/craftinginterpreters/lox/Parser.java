1b3bcbc733091171aacc6dd8f2b0a7776443c02a
Benson Lee <BensonL1334@gmail.com>
2023-07-19 23:09:17 -0400

    Add equality rule

--- com/craftinginterpreters/lox/Parser.java
+++ com/craftinginterpreters/lox/Parser.java
@@ -15,4 +15,16 @@ class Parser {
   private Expr expression() {
     return equality();
   }
+
+  private Expr equality() {
+    Expr expr = comparison();
+
+    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparison();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
 }
